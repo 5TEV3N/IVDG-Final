a11556d4aa6fc94c7ba40c32fa2e7db89a21dedc
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-01-24 19:32:47 -0500

    Added Player movement and various other Materials
    Also moved the mic stuff into refferences

---
+++ Assets/Scripts/Movement/InputManager3D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager3D : MonoBehaviour
+{
+    PlayerController3D playerController3D;       // refference to the playerController3D script
+
+    public Sprite playerSprite;                  // sprite of the player character
+
+    float xAxis = 0;                             // 1 = right, -1 = left
+    float zAxis = 0;                             // 1 = front, -1 back
+    float mouseXAxis = 0;                        // left or right movement of mouse (camera). Positive numb = right, Negative numb = left
+    float mouseYAxis = 0;                        // up or down movement of mouse (camera). Positive numb = up, Negative numb = down.
+    bool cameraLock = true;                      // constantly lock the cursor in the center
+
+    void Awake()
+    {
+        playerController3D = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController3D>();
+    }
+
+    void FixedUpdate()
+    { 
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        mouseXAxis = Input.GetAxis("Mouse X");
+        mouseYAxis = Input.GetAxis("Mouse Y");
+
+        xAxis = Input.GetAxisRaw("Horizontal");
+        zAxis = Input.GetAxisRaw("Vertical");
+
+        if (mouseXAxis != 0 || mouseYAxis != 0)
+        {
+            playerController3D.Mouselook(mouseXAxis, mouseYAxis);
+        }
+
+        if (xAxis != 0 || zAxis != 0)
+        {
+            playerController3D.PlayerMove(xAxis, zAxis);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cameraLock = true;
+
+            if (cameraLock == true)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                cameraLock = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerController3D.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController3D : MonoBehaviour
+{
+    InputManager3D inputManager;
+
+    [Header("Values")]
+    public float mouseSensitivity = 1;                               // Mouse sensitivity
+    public float jumpHeightIntensifier = 1;                          // How far i can jump
+    public float playerSpeedIntensifier = 1;                         // We can controll the speed of the player here.
+    public float upDownRange = 90.0f;                                // How far i can look up or down.
+
+    public float valOfVelocity;                                      // Checks how fast the player goes
+    public float maxVelocity;                                        // The max speed of how fast the player goes
+
+    [Header("Containers")]
+    public Rigidbody rb;                                             // Access the rigidbody to move
+    public Camera cam;                                               // Acess the Camera of the gameobject
+
+    private float verticalRotation = 0;                              // Contains the MouseYAxis
+    private float originalMaxVelocity;                               // Contains the orginal MaxVelocity  
+
+    void Awake()
+    {
+        inputManager = GameObject.FindGameObjectWithTag("Player").GetComponent<InputManager3D>();
+    }
+    
+    void Update()
+    {
+        valOfVelocity = rb.velocity.magnitude;
+    }
+
+    public void PlayerMove(float xAxis, float zAxis)
+    {
+
+        if (xAxis != 0)
+        {
+            if (xAxis > 0)
+            {
+                if (Mathf.Abs(valOfVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(transform.right * playerSpeedIntensifier);
+                }
+            }
+
+            if (xAxis < 0)
+            {
+                if (Mathf.Abs(valOfVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(-transform.right * playerSpeedIntensifier)  ;
+                }
+            }
+        }
+
+        if (zAxis != 0)
+        {
+            if (zAxis > 0)
+            {
+                if (Mathf.Abs(valOfVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(transform.forward * playerSpeedIntensifier);
+                }
+            }
+
+            if (zAxis < 0)
+            {
+                if (Mathf.Abs(valOfVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(-transform.forward * playerSpeedIntensifier);
+                }
+            }
+        }
+    }
+
+    public void Mouselook(float mouseXAxis, float mouseYAxis)
+    {
+        //Filter Horizontal input
+        if (mouseXAxis != 0)
+        {
+            gameObject.transform.Rotate(new Vector3(0, mouseXAxis, 0));
+        }
+
+        //Filter Vertical input
+        if (mouseYAxis != 0)
+        {
+            verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;               //This section pretty much clamps your camera rotation idk why this works
+            verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+            cam.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        }
+    }
+}
