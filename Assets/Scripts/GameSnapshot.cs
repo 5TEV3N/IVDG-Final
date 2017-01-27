dbcfc36d026ef584e35c714b6adeb630eabb082d
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-01-27 18:38:50 -0500

    Started work on Ingame Screenshots, Added UI to Main

---
+++ Assets/Scripts/GameSnapshot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSnapshot : MonoBehaviour
+{
+    //public Image ScreenShot;
+
+    public Text persistentDataPath;
+    void Start()
+    {
+        //Application.CaptureScreenshot("Screenshot " + 1);
+        //Application.persistentDataPath;
+        persistentDataPath.text = Application.persistentDataPath;
+    }
+}
