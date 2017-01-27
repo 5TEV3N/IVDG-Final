c416e3ddfcac7223dffb6c65b1191df8740262dc
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-01-27 17:53:57 -0500

    Started working on the states of the birds

---
+++ Assets/Scripts/States/BirdState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdState : MonoBehaviour
+{
+    public GameObject currentBirdSpawned;           // the current bird in the scene
+    public AudioSource currentBirdCry;              // the cry of the current bird
+    public Animator currentBirdAnimator;            // access the bird's animation
+
+    void Awake()
+    {
+        currentBirdSpawned = GameObject.FindWithTag("Bird").gameObject;     // checks if there's a bird in the scene
+        
+            // this is subject to change, this is to get the information of the spawned bird
+        //currentBirdCry = currentBirdSpawned.GetComponent<AudioSource>();
+        //currentBirdAnimator = currentBirdSpawned.GetComponent<Animator>();        
+    }
+
+    public static string CurrentBirdState(string state)                     // bird states, player can interact with these states by using this function.
+    {
+        switch (state)
+        {
+            case "hidden":
+                print("State: hidden. Bird is hidden and singing");
+                break;
+            case "interacting":
+                print("State: interacting. Bird is out of hiding and his in plain view to the player");
+                break;
+            case "flyaway":
+                print("State: runaway. Bird flies away from the player because of reasons");
+                break;
+            default:
+                print("Bird State is null");
+                break;
+        }
+        return state;
+    }
+}
