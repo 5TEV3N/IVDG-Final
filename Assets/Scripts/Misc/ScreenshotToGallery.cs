89619a28e433301b35701a282907782c22590659
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-02-02 21:44:39 -0500

    Have a lead inregards to dynamically scale the screenshot gallery

--- Assets/Scripts/Misc/ScreenshotToGallery.cs
+++ Assets/Scripts/Misc/ScreenshotToGallery.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ScreenshotToGallery : MonoBehaviour
 {
-    // populate the list with how much slots there is
-    // get ahold of the slot's RawImage Component
-    // add the screenshot saved in GameScreenshot into these Components
-    // dynamically scale this?
+    //populate the list with how much slots there is
+    //get ahold of the slot's RawImage Component
+    //add the screenshot saved in GameScreenshot into these Components
+    //dynamically scale this?
+    public List<RawImage> screenshotSlot = new List<RawImage>();
 
-    public RawImage[] screenshotSlot;
     private int thumbnailIndex = 0;
+    private RawImage newThumbnail;
 
     public void AddThumbnail(byte[] screenshotBytes)
     {
-        if (thumbnailIndex < screenshotSlot.Length)                                                                         // PROBLEM, THE TEXTURE GETS DISTORTED WHEN SAVE, PLEASE FIX LATER
+        if (thumbnailIndex < screenshotSlot.Count)                                                                          //PROBLEM, THE TEXTURE GETS DISTORTED WHEN SAVE, PLEASE FIX LATER
         {
             Texture2D screenshotTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, true);           //make a new texture2d to put into the ui's raw image
             screenshotTexture.LoadRawTextureData(screenshotBytes);                                                          //fills the screenshotTexture with the data with the bytes of when the player first took the screenshot
@@ -22,6 +23,12 @@ public class ScreenshotToGallery : MonoBehaviour
 
             screenshotSlot[thumbnailIndex].texture = screenshotTexture;                                                     //add the texture into the screenshotSlot into the index number = thumbnailIndex
             thumbnailIndex++;                                                                                               //go to the next itteration
+        }
+
+        if (thumbnailIndex == screenshotSlot.Count)
+        {
+            screenshotSlot.Add(newThumbnail);
+            //add a raw Image Gameobject UI into the slot
         }
     }
 }
