59cebc36f075b4b7f546c6f92cc78bd80509a9ff
superko <lucas@superko.org>
2017-02-07 21:28:18 -0500

    Adapting audio spectrum to measure pitch

--- Assets/Refference/audio-test/AudioSpectrum.cs
+++ Assets/Refference/audio-test/AudioSpectrum.cs
@@ -4,44 +4,28 @@
 [RequireComponent (typeof(AudioSource))]
 
 public class AudioSpectrum : MonoBehaviour {	
-	// Debug visualizer
-//	void Update () {
-//		float[] spectrum = new float[512];
-//
-//		AudioListener.GetSpectrumData( spectrum, 0, FFTWindow.Rectangular );
-//
-//		for( int i = 1; i < spectrum.Length-1; i++ )
-//		{
-//			Debug.DrawLine( new Vector3( i - 1, spectrum[i] + 10, 0 ), new Vector3( i, spectrum[i + 1] + 10, 0 ), Color.red );
-//			Debug.DrawLine( new Vector3( i - 1, Mathf.Log( spectrum[i - 1] ) + 10, 2 ), new Vector3( i, Mathf.Log( spectrum[i] ) + 10, 2 ), Color.cyan );
-//			Debug.DrawLine( new Vector3( Mathf.Log( i - 1 ), spectrum[i - 1] - 10, 1 ), new Vector3( Mathf.Log( i ), spectrum[i] - 10, 1 ), Color.green );
-//			Debug.DrawLine( new Vector3( Mathf.Log( i - 1 ), Mathf.Log( spectrum[i - 1] ), 3 ), new Vector3( Mathf.Log( i ), Mathf.Log( spectrum[i] ), 3 ), Color.blue );
-//		}
-//	}
 
 	public GameObject prefab;
-	public int numBars = 64;
-	public float radius = 1f;
 	public GameObject[] bars;
+	private float radius = 0.1f;
+	private int[] freqArray = new int[] {13,14,15,16,17,18,19,20,21,22,24,25,27,28,30,32,33,35,38,40,42,45,47,50,53,56,60,63,67,71,75,80,84,89,95,100,106,113,119,126,134,142,150,159,169,179,189,200,212,225,238,253,268,284,300,318,337,357,378,401,425,450,477,505,535,567,601,636,674};
 
 	void Start () {
-		for (int i=0; i < numBars; i++) {
+		for (int i=0; i < freqArray.Length; i++) {
 			Vector3 position = new Vector3 (i*radius, 0, 0);
 			Instantiate (prefab, position, Quaternion.identity);
 		}
 		bars = GameObject.FindGameObjectsWithTag ("audiobars");
 	}
 
-
-
 	void Update () {
-		float[] spectrum = new float[numBars];
+		float[] spectrum = new float[2048];
 
-		AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+		AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
 
-		for (int i = 0; i < numBars; i++) {
+		for (int i = 0; i < freqArray.Length; i++) {
 			Vector3 prevScale = bars[i].transform.localScale;
-			prevScale.y = Mathf.Lerp (prevScale.y, spectrum [i] * 30, Time.deltaTime * 15);
+			prevScale.y = Mathf.Lerp (prevScale.y, spectrum[freqArray[i]] * 30, Time.deltaTime * 30);
 			bars[i].transform.localScale = prevScale;
 		}
 	}
