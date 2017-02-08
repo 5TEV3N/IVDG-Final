454adb909e588c307457b1584fb4b8d9c9a0fc3d
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-02-07 20:54:40 -0500

    Dynamically scaling screenshot gallery partially ready to go!
    Problem : indexing issue where the next page starts with the wrong screenshot

--- Assets/Scripts/Misc/ScreenshotToGallery.cs
+++ Assets/Scripts/Misc/ScreenshotToGallery.cs
@@ -7,39 +7,30 @@ public class ScreenshotToGallery : MonoBehaviour
     //Dynamically scale this
     public int thumbnailIndex = 0;
     public List<RawImage> screenshotSlot = new List<RawImage>();
-    public RawImage newThumbnail;
+    public Texture2D screenshotTexture;
     public GameObject screenshotPage;
-    public Transform canvasTransform;
-
-    private int pageCounter = 1;
-    private GameObject newScreenshotPage;
-    private RawImage newScreenshotPageComponents;
-
-    void Awake()
-    {
-        newScreenshotPageComponents = screenshotPage.GetComponentInChildren<RawImage>();
-    }
+    public Transform screenshotParentTransform;
+    private RawImage[] newPageSlotsComponents;
 
     public void AddThumbnail(byte[] screenshotBytes)
     {
-        if (thumbnailIndex == screenshotSlot.Count)
+        if (thumbnailIndex < screenshotSlot.Count)
         {
-            GameObject newScreenshotPage = Instantiate(screenshotPage, canvasTransform);
-            newScreenshotPage.name = "ScreenshotPage " + ++pageCounter;
-            //incorpriate the insantiated screenshotPage here
-            screenshotSlot.Add(newScreenshotPageComponents);
-            thumbnailIndex = 0;
-            //make it so that index is not the same number as the screenshotslot.count
-        }
-
-        if (thumbnailIndex < screenshotSlot.Count)                                                                          
-        {
-            Texture2D screenshotTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, true);           //make a new texture2d to put into the ui's raw image
+            screenshotTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, true);                     //make a new texture2d to put into the ui's raw image
             screenshotTexture.LoadRawTextureData(screenshotBytes);                                                          //fills the screenshotTexture with the data with the bytes of when the player first took the screenshot
             screenshotTexture.Apply();                                                                                      //apply the data to the texture
 
             screenshotSlot[thumbnailIndex].texture = screenshotTexture;                                                     //add the texture into the screenshotSlot into the index number = thumbnailIndex
             thumbnailIndex++;                                                                                               //go to the next itteration
+        }
+        else
+        {                                            
+            screenshotPage = Instantiate(screenshotPage,screenshotParentTransform, false);                                  //instantiate a new page
+            for (int i = 0; i < screenshotPage.transform.childCount; i++)                                                   //gets the components inside of the pages
+            {
+                newPageSlotsComponents = screenshotPage.GetComponentsInChildren<RawImage>();
+            }
+            screenshotSlot.AddRange(newPageSlotsComponents);                                                                //add those components into the new pages
         }
     }
 }
