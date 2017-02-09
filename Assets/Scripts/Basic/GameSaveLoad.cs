8603636b1b8a31899b271de041d507e793fd5517
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-02-08 21:46:17 -0500

    Started working on the Saving + Loading System. More Under the cut
    WARNING: RENAMED MISC INTO SCREENSHOT
    Added UI prefab
    Added a super basic UI system

---
+++ Assets/Scripts/Basic/GameSaveLoad.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+class PlayerData
+{
+    public Vector3 savedPlayerPosition;
+    //add the lists of screenshots in a gallery here.
+}
+
+public class GameSaveLoad : MonoBehaviour
+{
+    public static GameSaveLoad gameState;
+    public Vector3 currentPlayerPosition;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            currentPlayerPosition = player.transform.position;
+        }
+    }
+
+    public void PlayerSave()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+        PlayerData data = new PlayerData();
+        data.savedPlayerPosition = currentPlayerPosition;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void PlayerLoad()
+    {
+        if(File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            currentPlayerPosition = data.savedPlayerPosition;
+        }
+    }
+
+    public void ScreenshotSave()
+    {
+
+    }
+
+    public void ScreenshotLoad()
+    {
+
+    }
+}
+//REFFERENCE
+//https://unity3d.com/learn/tutorials/topics/scripting/persistence-saving-and-loading-data
---
+++ Assets/Scripts/Basic/GameUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject gamePause;
+    public GameObject TitleScreen;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePause != null)
+            {
+                gamePause.SetActive(false);
+            }
+            else
+            {
+                gamePause.SetActive(true);
+            }
+        }	
+	}
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Main");
+        print("New Game");
+    }
+
+    public void LoadGamePlayScene()
+    {
+        SceneManager.LoadScene("Main");
+        GameSaveLoad.gameState.PlayerLoad();
+        print("Loaded Game");
+    }
+
+    public void ExitGame()
+    {
+        GameSaveLoad.gameState.PlayerSave();
+        print("Saved the Game!");
+
+        Application.Quit();
+    }
+}
