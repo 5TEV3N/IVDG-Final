4f6bf6bd166321e489ab7a527f82670b0bb9f4ca
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-02-15 21:18:41 -0500

    Implamented Player Raycasting
    Interactive player objects must be in the Interactive Layer Mask

---
+++ Assets/Scripts/Player/PlayerRaycast.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public float mouseRayDistance;
+
+    private RaycastHit mouseHit;
+    private Ray mouseRay;
+    private Vector3 mousePosition;
+    private LayerMask interactiveMask;
+
+    void Awake()
+    {
+        interactiveMask = LayerMask.GetMask("Interactive");
+    }
+
+    void Update()
+    {
+        PlayerRaycastManager();
+        PlayerInteraction();
+    }
+
+    void PlayerRaycastManager()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(mousePosition, mouseRay.direction * mouseRayDistance, Color.green);
+    }
+
+    public void PlayerInteraction()
+    {
+        if (Physics.Raycast(mouseRay,out mouseHit,mouseRayDistance,interactiveMask))
+        {
+            print("hit");
+        }
+    }
+}
