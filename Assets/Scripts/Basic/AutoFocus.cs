226b7f40e6d45c3aa1fa92959c99034a7400eb78
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-03-29 12:53:48 -0400

    Auto Focus for the trailer is added

---
+++ Assets/Scripts/Basic/AutoFocus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class AutoFocus : MonoBehaviour
+{
+    public DepthOfField dof;
+
+	void Start ()
+    {
+        InvokeRepeating("RefocusUpdate", 0, 0.1f);		
+	}
+
+	void RefocusUpdate ()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            dof.focalLength = hit.distance;        
+        }
+	}
+}
