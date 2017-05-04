475245500dc9cd6fd4817b8984f42ea0e56d9733
Ana TM <ana.taveramendoza@gmail.com>
2017-05-03 20:39:44 -0400

    environment stuff

---
+++ Assets/Refference/LowPolyWater/Scripts/src/LPWEdgeBlend.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class LPWEdgeBlend : MonoBehaviour {
+
+    public void OnWillRenderObject() {
+        if (!enabled || !GetComponent<Renderer>()) return;
+        var material = GetComponent<Renderer>().sharedMaterial;
+        if (!material) return;
+
+        Camera cam = Camera.current;
+        if (!cam) return;
+
+        if (!material.HasProperty("_EdgeBlend")) return;
+
+        if (material.GetFloat("_EdgeBlend") > 0.1f)
+            cam.depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+}
