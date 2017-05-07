8f0de14aad8fbebeacc40b39f722afef7dd21e34
superko <lucas@superko.org>
2017-05-06 20:45:49 -0400

    Added raw audio files, not implemented in-scene.

---
+++ Assets/Scripts/AudioSFX/Wingflaps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wingflaps : MonoBehaviour {
+
+	public AudioClip[] listOfSongs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
